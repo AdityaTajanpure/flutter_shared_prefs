0349fc073fcfaa3f149380962c6fd34fe432b18f
Aditya Tajanpure <67155550+AdityaTajanpure@users.noreply.github.com>
2021-08-11 12:00:43 +0530

    Implemented SharedPrefs

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(
-        color: Colors.redAccent,
-      ),
+      home: MyHomePage(),
     );
   }
 }
--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -1,61 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyHomePage extends StatefulWidget {
-  final Color color;
-  const MyHomePage({Key key, @required this.color}) : super(key: key);
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Color _curColor;
+  final Map<String, Color> colors = {
+    'redAccent': Colors.redAccent,
+    'yellowAccent': Colors.yellowAccent,
+    'pinkAccent': Colors.pinkAccent,
+    'blueAccent': Colors.blueAccent,
+    'cyanAccent': Colors.cyanAccent,
+    'amberAccent': Colors.amberAccent,
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _getColor();
+  }
+
+  void _getColor() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String colorName = prefs.getString("color");
+    setState(() {
+      _curColor = colors[colorName];
+    });
+  }
+
+  void _setColor(String name, Color color) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("color", name);
+    setState(() {
+      _curColor = color;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Flutter shared prefs demo"),
-          backgroundColor: widget.color,
+          backgroundColor: _curColor,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Current Saved Color is: ${widget.color}"),
-            MyColouredContainer(
-              color: Colors.redAccent,
+            // Text("Current Saved Color is: ${prefs.getString("color")}"),
+            SizedBox(
+              height: 50,
             ),
-            MyColouredContainer(
-              color: Colors.yellowAccent,
-            ),
-            MyColouredContainer(
-              color: Colors.pinkAccent,
-            ),
-            MyColouredContainer(
-              color: Colors.blueAccent,
-            ),
-            MyColouredContainer(
-              color: Colors.cyanAccent,
-            ),
-            MyColouredContainer(
-              color: Colors.amberAccent,
-            ),
+            for (var entry in colors.entries)
+              InkWell(
+                onTap: () => _setColor(entry.key, entry.value),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  color: entry.value,
+                ),
+              )
           ],
         ));
-  }
-}
-
-class MyColouredContainer extends StatelessWidget {
-  final Color color;
-  const MyColouredContainer({
-    Key key,
-    @required this.color,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      color: this.color,
-    );
   }
 }
